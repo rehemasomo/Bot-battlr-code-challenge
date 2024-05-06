11bd89af98327926dc84480ed9c06c3f926db872
rehemasomo <rehemasomo30@gmail.com>
2024-05-07 00:33:39 +0300

    updated url

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -8,7 +8,7 @@ function BotsPage() {
   const [userArmy, setUserArmy] = useState([]);
   
   useEffect(() => {
-    fetch("http://localhost:8002/bots")
+    fetch("https://bot-battlr-code-challenge-1.onrender.com")
       .then((res) => res.json())
       .then((data) => setBots(data))
       .catch((err) => console.error("Error fetching bots:", err));
@@ -27,7 +27,7 @@ function BotsPage() {
   const dischargeBot = (bot) => {
     setUserArmy(userArmy.filter((b) => b.id !== bot.id));
   
-    fetch(`http://localhost:8002/bots/${bot.id}`, {
+    fetch(`https://bot-battlr-code-challenge-1.onrender.com`, {
       method: "DELETE"
     })
     .then(response => {
