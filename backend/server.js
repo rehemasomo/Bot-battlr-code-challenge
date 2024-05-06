b244f2a7bbaff71ad91f2a8f54f0f90d89b3e2cd
rehemasomo <rehemasomo30@gmail.com>
2024-05-07 00:17:32 +0300

    completed

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,6 @@ const middlewares = jsonServer.defaults()
  
 server.use(middlewares)
 server.use('/', router)
-server.listen(process.env.PORT || 3000, () => {
+server.listen(process.env.PORT || 8000, () => {
   console.log('JSON Server is running')
 })
